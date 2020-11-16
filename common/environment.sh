e6689c3bb042ffd7542fb39e83911dfcad402fa8
LavendaRaphael <feifei.tian.cn@gmail.com>
2020-11-16 14:30:59 +0800

    x

--- common/environment.sh
+++ common/environment.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 #set -eo pipefail
 
+myserver="none"
 source ~/tianff/server/server.sh
+if [ "$myserver" = "none" ]; then
+    echo "$myserver"
+    return
+fi
+
 
 export VIMINIT='source ~/tianff/codes/common/vimrc.me'
 cdl() {
@@ -10,19 +16,15 @@ cdl() {
 }
 alias cpi="cp -i"
 
+#======================================[ssh-agent]
 env=~/.ssh/agent.env
-
 agent_load_env () { test -f "$env" && . "$env" >| /dev/null ; }
-
 agent_start () {
     (umask 077; ssh-agent >| "$env")
     . "$env" >| /dev/null ; }
-
 agent_load_env
-
 # agent_run_state: 0=agent running w/ key; 1=agent w/o key; 2= agent not running
 agent_run_state=$(ssh-add -l >| /dev/null 2>&1; echo $?)
-
 if [ ! "$SSH_AUTH_SOCK" ] || [ $agent_run_state = 2 ]; then
     agent_start
     ssh-add ~/tianff/server/id_ed25519
@@ -31,7 +33,6 @@ elif [ "$SSH_AUTH_SOCK" ] && [ $agent_run_state = 1 ]; then
     ssh-add ~/tianff/server/id_ed25519
     ssh-add ~/tianff/server/id_rsa
 fi
-
 unset env
 
 #======================================[MYUBUNTU]
