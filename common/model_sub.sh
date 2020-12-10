4bfc16a970b341c57bc7a58b358de2aa68db65f4
LavendaRaphael <feifei.tian.cn@gmail.com>
2020-12-10 20:15:58 +0800

    shtu

--- common/model_sub.sh
+++ common/model_sub.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-set -eo pipefail
-source ~/tianff/codes/202011_XasWater32Qe/local_env.sh
+set -euo pipefail
+source ~/tianff/codes/common/environment.sh
 
-jobname=pbexNUMx
-ncore=6
+jobname=test
+ncore=36
 source ~/tianff/codes/common/sub_head.sh
 #========================================[main script]
 cat >> ${jobname}.sh<<eof
+mpirun ${software_bin}intelmpi_test.x
 
 echo "TotalTime \$((\${SECONDS} / 60)) m \$((\${SECONDS} % 60)) s."
 eof
