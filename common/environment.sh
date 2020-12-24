0e6b2bcfe76a53f6038d05e8bd2b521bb0bf5c30
LavendaRaphael <feifei.tian.cn@gmail.com>
2020-12-24 22:26:24 +0800

    kunlun

--- common/environment.sh
+++ common/environment.sh
@@ -47,6 +47,8 @@ module load mpi/intelmpi/2017.4.239
 module load mathlib/lapack/intel/3.8.0
 module add apps/gnuplot/5.0.5/gcc-7.3.1
 module list
+vasp_pot=~/tianff/software/vasp/potpaw_PBE.54/
+echo "vasp_pot=${vasp_pot}"
 
 #======================================[SPST]
 elif [ "$myserver" = "SPST" ]; then
