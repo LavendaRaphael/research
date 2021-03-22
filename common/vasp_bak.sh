72a0ad1398b838e58e8d42ef595cc964cfa92e43
LavendaRaphael <feifei.tian.cn@gmail.com>
2021-03-22 14:57:02 +0800

    kunlun

--- common/vasp_bak.sh
+++ common/vasp_bak.sh
@@ -2,23 +2,25 @@
 set -eo pipefail
 #source ~/tianff/scripts/environment.sh
 
-rm -f *.xsd */*.xsd
-rm -f CHG */CHG
-rm -f CHGCAR */CHGCAR
-#rm -f CONTCAR */CONTCAR
-rm -f DOSCAR */DOSCAR
-rm -f EIGENVAL */EIGENVAL
-rm -f IBZKPT */IBZKPT
-#rm -f INCAR */INCAR
-#rm -f KPOINTS  */KPOINTS
-rm -f OSZICAR */OSZICAR
-rm -f OUTCAR */OUTCAR
-rm -f PCDAT */PCDAT
-#rm -f POSCAR */POSCAR
-rm -f POTCAR */POTCAR
-rm -f PROCAR */PROCAR
-rm -f REPORT */REPORT
-rm -f vasprun.xml */vasprun.xml
-rm -f WAVECAR */WAVECAR
-rm -f XDATCAR */XDATCAR
-rm -f STOPCAR
+ rm -f *.xsd        */*.xsd
+ rm -f CHG          */CHG
+ rm -f CHGCAR       */CHGCAR
+#rm -f CONTCAR      */CONTCAR
+ rm -f DOSCAR       */DOSCAR
+ rm -f EIGENVAL     */EIGENVAL
+ rm -f IBZKPT       */IBZKPT
+#rm -f INCAR        */INCAR
+#rm -f KPOINTS      */KPOINTS
+ rm -f model_sub.sh */model_sub.sh
+ rm -f OSZICAR      */OSZICAR
+ rm -f OUTCAR       */OUTCAR
+ rm -f PCDAT        */PCDAT
+#rm -f POSCAR       */POSCAR
+ rm -f POTCAR       */POTCAR
+ rm -f PROCAR       */PROCAR
+ rm -f REPORT       */REPORT
+ rm -f vasprun.xml  */vasprun.xml
+#rm -f vasp.log     */vasp.log
+ rm -f WAVECAR      */WAVECAR
+ rm -f XDATCAR      */XDATCAR
+ rm -f STOPCAR      */STOPCAR
