a5bfcda050875e8a655f23ed38b1b06dfb53aaf9
LavendaRaphael <feifei.tian.cn@gmail.com>
2021-03-31 19:44:41 +0800

    change common codes

--- common/chmod.me.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -eo pipefail
-
-find  ./  -maxdepth 1 -type d -exec chmod 750 {} \;
-find  ./  -maxdepth 1 -type f -exec chmod 640 {} \;
-find  ./  -maxdepth 1 -name "*.sh"  -exec chmod 750 {} \;
