e09f6623a159b0a7427396ee1b327f0cf86bccec
LavendaRaphael <feifei.tian.cn@gmail.com>
2021-04-19 16:55:24 +0800

    job_init.sh mod POSCAR auto

--- 202103_XasPtO/job_init.sh
+++ 202103_XasPtO/job_init.sh
@@ -7,12 +7,21 @@ cd $work_dir
 for i in $loopfile
 do
     echo $i
-	rm -rf O_${i}
-	mkdir O_${i}
-	cp template/* O_${i}/
-	cd O_${i}/
-    awk 'NR-"'${i}'"==0{print}' snap.pos >> POSCAR
-    awk 'NR-"'${i}'"!=0{print}' snap.pos >> POSCAR
-    rm snap.pos
+    rm -rf O_${i}
+    mkdir O_${i}
+    cp template/* O_${i}/
+    cd O_${i}/
+
+    num1=$(awk 'NR-7==0 {print $1}' POSCAR)
+    if [ "$num1" != "1" ];then
+        awk 'NR-6==0{$1=$1" "$1}1' POSCAR > tmp && mv tmp POSCAR
+        awk 'NR-7==0{$1=1" "$1-1}1' POSCAR > tmp && mv tmp POSCAR
+    fi
+    num2=$(awk 'NR-8>=0 && NR-9<=0 && $1 ~ /^D/ {print NR}' POSCAR)
+    line1=$(awk 'NR-"'$(($num2+1))'"==0 {print} ' POSCAR)
+    line2=$(awk 'NR-"'$(($num2+$i))'"==0 {print} ' POSCAR)
+    sed -i "$(($num2+1))c $line2" POSCAR
+    sed -i "$(($num2+$i))c $line1" POSCAR
+
     cd ..
 done
