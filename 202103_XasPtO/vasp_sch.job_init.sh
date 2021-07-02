7fa7dcf61221fd328387679e4e0b088f866768e9
LavendaRaphael <feifei.tian.cn@gmail.com>
2021-07-02 11:45:38 +0800

    shtu

--- 202103_XasPtO/vasp_sch.job_init.sh
+++ 202103_XasPtO/vasp_sch.job_init.sh
@@ -7,17 +7,25 @@ cd $work_dir
 for i in $loopfile
 do
     echo $i
-    if [ ! -d "$var" ]; then
+    if [ ! -d "atom_${i}" ]; then
         mkdir atom_${i}
     fi
+
+#   ls tempalte/
+#   $ INCAR POTCAR vasp_sub.sh
     cp template/* atom_${i}/
     cd atom_${i}/
 
+#6     O   O   X   
+#7     1   3   3
     num1=$(awk 'NR-7==0 {print $1}' POSCAR)
     if [ "$num1" != "1" ];then
         awk 'NR-6==0{$1=$1" "$1}1' POSCAR > tmp && mv tmp POSCAR
         awk 'NR-7==0{$1=1" "$1-1}1' POSCAR > tmp && mv tmp POSCAR
     fi
+
+#8      Select
+#9      Direct
     num2=$(awk 'NR-8>=0 && NR-9<=0 && $1 ~ /^D/ {print NR}' POSCAR)
     line1=$(awk 'NR-"'$(($num2+1))'"==0 {print} ' POSCAR)
     line2=$(awk 'NR-"'$(($num2+$i))'"==0 {print} ' POSCAR)
