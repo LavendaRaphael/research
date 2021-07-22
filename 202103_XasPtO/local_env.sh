972856fe94e6ffff3f206a530aaffe89d561db67
LavendaRaphael <feifei.tian.cn@gmail.com>
2021-07-22 13:38:57 +0800

    shtu

--- 202103_XasPtO/local_env.sh
+++ 202103_XasPtO/local_env.sh
@@ -13,13 +13,13 @@ echo "--------------------------------------------------------------------------
 # work_dir=${gotowork_3}vasp_sch/
 # work_dir=${homedir}group/202103_XasPtO/server/Pt-111_O_vac/Pt-111a4b4c4_O4_vac15/aimd2000.qe_fch_scf/
 # work_dir=${homedir}group/202103_XasPtO/server/Pt-111_O_vac/Pt-111a4b4c4_O4_vac15/aimd2000.qe_hch_scf/
-  work_dir=${gotowork_2}sch_aimd3_snap329/
+# work_dir=${gotowork_2}sch_aimd3_snap329/
 # work_dir=${homedir}group/202103_XasPtO/server/Pt-110_O_vac/Pt-110a12b2c4.5_O22_vac15/qe_fch_scf/
 # work_dir=${homedir}group/202103_XasPtO/server/Pt-110_O_vac/Pt-110a12b2c4.5_O22_vac15/qe_hch_scf/
-# work_dir=${homedir}group/202103_XasPtO/server/Pt-110_O_vac/Pt-110a12b2c4.5_O22_vac15/vasp_sch/
+  work_dir=${gotowork_2}vasp_sch/
 echo "work_dir=$work_dir"
 
-  O_num=$(awk 'NR-7==0 {print $1}' ${work_dir}template/POSCAR)
+# O_num=$(awk 'NR-7==0 {print $1}' ${work_dir}template/POSCAR)
 # echo "O_num=$O_num"
 # O_num=4
 
@@ -36,10 +36,10 @@ echo "subfile=$subfile"
 
 # loopfile="1"
 # loopfile=`seq 1 $O_num`
-# loopfile="1 3 5 7 9 11"
+  loopfile="1 3 5 7 9 11"
 # loopfile="1 1 3 3 5 5 7 7 9 9 11"
 # loopfile="1 3"
-  loopfile="3 4 5 11 12 13 14 15 16 17 18 19 20 21 22"
+# loopfile="3 4 5 11 12 13 14 15 16 17 18 19 20 21 22"
 echo "loopfile=$loopfile"
 
   tensorxyz="X Y Z"
