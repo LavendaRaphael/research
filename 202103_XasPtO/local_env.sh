f2050e64182ead41381728dfb3d697d66ce614b4
LavendaRaphael <feifei.tian.cn@gmail.com>
2021-08-26 13:42:46 +0800

    ubuntu

--- 202103_XasPtO/local_env.sh
+++ 202103_XasPtO/local_env.sh
@@ -10,34 +10,23 @@ fi
 fi
 local_env=false
 echo "#=========================================================================[local_env.sh]"
-# work_dir=${goto_pto_work_110}Pt.110.x12y2z4.5_O22_vac15/
-# work_dir=${goto_pto_work_110}Pt.110.x12y2z4.5_O22_vac15/vasp_sch.aimd2_932/
-# work_dir=${goto_pto_work_110}Pt.110.x2y3z4.5_O6_vac15/
-# work_dir=${goto_pto_work_110}Pt.110.x2y3z4.5_O1_vac15/
-# work_dir=${goto_pto_work_110}Pt.110.x2y3z4.5_O2x_vac15/
-# work_dir=${goto_pto_work_110}Pt.110.x2y3z4.5_O2y_vac15/
- work_dir=${goto_pto_work_110}Pt.110.x2y3z4.5_O2.14_vac15/
-# work_dir=${goto_pto_work_110}Pt.110.x2y3z4.5_O3xy_vac15/
-# work_dir=${goto_pto_work_110}Pt.110.x2y3z4.5_O3y_vac15/
-# work_dir=${goto_pto_work_110}Pt.110.x2y3z4.5_O3.136_vac15/
-# work_dir=${goto_pto_work_110}Pt.110.x2y4z4.5_O4_vac15/
+# work_dir=Pt.110.x12y2z4.5_O22_vac15/
+# work_dir=Pt.110.x12y2z4.5_O22_vac15/vasp_sch.aimd2_932/
+# work_dir=Pt.110.x2y3z4.5_O6_vac15/
+# work_dir=Pt.110.x2y3z4.5_O1_vac15/
+# work_dir=Pt.110.x2y3z4.5_O2.12_vac15/
+# work_dir=Pt.110.x2y3z4.5_O2.13_vac15/
+  work_dir=Pt.110.x2y3z4.5_O2.14_vac15/
+# work_dir=Pt.110.x2y3z4.5_O3.123_vac15/
+# work_dir=Pt.110.x2y3z4.5_O3.135_vac15/
+# work_dir=Pt.110.x2y3z4.5_O3.136_vac15/
+# work_dir=Pt.110.x2y4z4.5_O4_vac15/
+ work_dir=${goto_pto_work_110}${work_dir}
  work_dir=${work_dir}vasp_sch/
 echo "work_dir=$work_dir"
 
 # O_num=$(awk 'NR-7==0 {print $1}' ${work_dir}template/POSCAR)
 # echo "O_num=$O_num"
-# O_num=4
-
-# sub_dir="xspectra/"
-# sub_dir="xspectra.epsilon010/"
-# sub_dir="xspectra.epsilon100/"
-  sub_dir=""
-echo "sub_dir=$sub_dir"
-
-# subfile=xspectra_sub.sh
-  subfile=vasp_sub.sh
-# subfile=scf_sub.sh
-echo "subfile=$subfile"
 
  loopfile=(1)
 # loopfile=`seq 1 $O_num`
@@ -46,6 +35,17 @@ echo "subfile=$subfile"
 # loopfile=(1 3)
 # loopfile=(1 1 3)
  echo "loopfile=$loopfile"
+
+  subfile=vasp_sub.sh
+# subfile=xspectra_sub.sh
+# subfile=scf_sub.sh
+echo "subfile=$subfile"
+
+# sub_dir="xspectra/"
+# sub_dir="xspectra.epsilon010/"
+# sub_dir="xspectra.epsilon100/"
+  sub_dir=""
+echo "sub_dir=$sub_dir"
 
 echo "#=========================================================================<<<"
 
