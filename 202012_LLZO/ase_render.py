68e5ddd9f0c1858b7180e13dcd706d354a24dca6
LavendaRaphael <feifei.tian.cn@gmail.com>
2021-09-13 20:12:48 +0800

    auto

--- 202012_LLZO/ase_render.py
+++ 202012_LLZO/ase_render.py
@@ -5,14 +5,24 @@ import sys
 import os
 
 cont=read(str(sys.argv[1]))
+cell=cont.cell
+cont=cont*(1,1,1)
+cont.cell=cell
 
 generic_projection_settings = {
     'show_unit_cell': 2,
+#    'rotation': '90x',
     'rotation': '90y',
+#    'rotation': '90x,90z'
+#    'rotation': '10x,-10y'
+#    'rotation': '-72x'
 }
 
 povray_settings={
     'textures': ['simple',]*1000,
+#    'canvas_width': 500,
+    'canvas_height': 500,
+    'celllinewidth': 0.03
 }
 renderer=write('render.pov',cont,
     **generic_projection_settings,
