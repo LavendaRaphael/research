1c5dd8e1b4fe5ddf7bc5dce866f1a487f848be0d
LavendaRaphael <feifei.tian.cn@gmail.com>
2021-09-13 20:24:56 +0800

    auto

--- 202012_LLZO/local_env.sh
+++ 202012_LLZO/local_env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 homedir=`find ~ -maxdepth 3 -name "server.me.sh" -print -quit|xargs dirname`/
-source ${homedir}codes/common/environment.sh
+source ${homedir}codes/group/common/groupenv.sh
 set -euo pipefail
 
 if [ ! -z ${local_env+x} ]; then
@@ -10,17 +10,16 @@ fi
 fi
 local_env=false
 echo "---------------------------------------------------------------------------[local_env.sh]"
-# work_dir=${goto_llzo_li}Li.a1b1c1_eosfit/
-# work_dir=${goto_llzo_li}Li.a1b1c1_eosfit.PBE-D3/
-# work_dir=${goto_llzo_li}Li.a1b1c1_eosfit.optB88-vdW/
-# work_dir=${goto_llzo_au}Au.a1b1c1_eosfit/
- work_dir=${goto_llzo}Li7La3Zr2O12/Li7La3Zr2O12.a1b1c1_eosfit/
+# work_dir=${goto_llzo_li}Li.x1y1z1_eosfit/
+ work_dir=${goto_llzo_work}Li/Li.x1y1z1_eosfit.PBE-D3/
+# work_dir=${goto_llzo_li}Li.x1y1z1_eosfit.optB88-vdW/
+# work_dir=${goto_llzo_au}Au.x1y1z1_eosfit/
+# work_dir=${goto_llzo}Li7La3Zr2O12/Li7La3Zr2O12.x1y1z1_eosfit/
 echo "work_dir=$work_dir"
 
   subfile=vasp_sub.sh
 echo "subfile=$subfile"
 
-# loopfile=(0.91 0.92 0.93 0.95 0.97 0.98 0.99 1.00 1.01 1.03)
  loopfile=$(seq 0.90 0.01 1.10)
 echo "loopfile=$loopfile"
 
