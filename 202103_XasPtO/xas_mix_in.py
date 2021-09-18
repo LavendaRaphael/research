057e17122c31e63838b9a698c647ee06e09aae98
LavendaRaphael <feifei.tian.cn@gmail.com>
2021-09-18 16:02:17 +0800

    auto

--- 202103_XasPtO/xas_mix_in.py
+++ 202103_XasPtO/xas_mix_in.py
@@ -1,16 +1,40 @@
 #!/bin/env python
 import math
+from from_xas_mix import def_xas_mix
 
-class cla_paras:
-    alpha = 20
-    beta = 90.0
-    alpha = alpha / math.pi
-    beta = beta / math.pi
-    weight_1 = 0.5 * (math.cos(beta)**2 + math.cos(beta)**2 * math.sin(alpha)**2)
-    weight_2 = weight_1
-    weight_3 = math.cos(alpha)**2 * math.sin(beta)**2
-    list_files = []
-    # filename, column[n], weight
-    list_files.append( ('xas_ave.dat', 1, weight_1) )
-    list_files.append( ('xas_ave.dat', 2, weight_2) )
-    list_files.append( ('xas_ave.dat', 3, weight_3) )
+def def_weight (alpha,beta)
+    alpha = math.radians (alpha)
+    beta = math.radians (beta)
+    result = []
+    result.append (0.5 * (math.cos(beta)**2 + math.cos(beta)**2 * math.sin(alpha)**2))
+    result.append (result[0])
+    result.append (math.cos(alpha)**2 * math.sin(beta)**2)
+
+alpha = 20
+float_frac = 0.7
+
+beta = 90
+str_prefix = 'xas_mix.a' +str(alpha) +'_b' +str(beta)
+list_weight = def_weight (alpha,beta)
+list_weight_1 = list_weight
+list_files = []
+list_files.append( ('xas_ave.dat', 1, list_weight[0]) )
+list_files.append( ('xas_ave.dat', 2, list_weight[1]) )
+list_files.append( ('xas_ave.dat', 3, list_weight[2]) )
+
+beta = 45.0
+str_prefix = 'xas_mix.a' +str(alpha) +'_b' +str(beta)
+list_weight = def_weight (alpha,beta)
+list_weight_2 = list_weight
+list_files = []
+list_files.append( ('xas_ave.dat', 1, list_weight[0]) )
+list_files.append( ('xas_ave.dat', 2, list_weight[1]) )
+list_files.append( ('xas_ave.dat', 3, list_weight[2]) )
+
+str_prefix = 'xas_mix.a' +str(alpha) +'_frac'
+list_weight = [ list_weight_1[i]*float_frac + list_weight_2[i]*(1.0-float_frac) for i in range(3) ]
+list_files = []
+list_files.append( ('xas_ave.dat', 1, list_weight[0]) )
+list_files.append( ('xas_ave.dat', 2, list_weight[1]) )
+list_files.append( ('xas_ave.dat', 3, list_weight[2]) )
+
