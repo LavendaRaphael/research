3f59ae38934522a73294181d63fdb581f008d2e3
LavendaRaphael <feifei.tian.cn@gmail.com>
2021-09-19 17:26:41 +0800

    auto

--- common/from_xas_modules.py
+++ common/from_xas_modules.py
@@ -4,6 +4,43 @@
 # 2021.09.18
 #===============================================<<
 
+
+def def_xas_findpeaks(str_file, float_relheight):
+#------------------------------[]
+#------------------------------[]
+    str_prefix = 'xas_findpeaks'
+    str_logfile = str_prefix + '.log'
+    str_outfile = str_prefix + '.dat'
+
+    obj_logfile = open(str_logfile,'w')
+    print("#--------------------[xas_findpeaks]\n")
+
+    list_xas_e=[]
+    list_xas_i=[]
+
+    for str_line in open(str_file):
+        if ((not str_line.strip()) or str_line.strip().startswith("#")):
+            continue
+        list_xas_e.append (str_line.split()[0])
+        list_xas_i.append (float(str_line.split()[1]) )
+    int_lenxas=len(list_xas_e)
+    obj_logfile.write(f'int_lenxas {int_lenxas}\n')
+
+
+    obj_outfile = open(str_outfile, 'w')
+    obj_outfile.write(f'#   Energy  Intensity\n')
+    int_count = 0
+    for int_count in range(int_lenxas):
+        obj_outfile.write(f'{list_xas_e[int_count]} {list_xas_i[int_count]}\n')
+        int_count += 1
+    obj_outfile.close()
+
+    print("#--------------------<<\n")
+    obj_logfile.close()
+
+    return
+
+
 def def_xas_mix(list_files, str_prefix):
 #------------------------------[]
 # str_outfile = 'xas_mix'
