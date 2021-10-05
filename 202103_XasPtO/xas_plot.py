c4d34d83c25ee05b133ba258bdcc4f21e9c8ac06
LavendaRaphael <feifei.tian.cn@gmail.com>
2021-10-05 17:10:22 +0800

    auto

--- 202103_XasPtO/xas_plot.py
+++ 202103_XasPtO/xas_plot.py
@@ -4,19 +4,27 @@ import os
 import numpy
 import json
 import matplotlib.pyplot as plt
+import matplotlib
+
+print(matplotlib.matplotlib_fname())
+
+matplotlib.rcParams['font.size']=25
+matplotlib.rcParams['font.family']='sans-serif'
+matplotlib.rcParams['font.sans-serif']=["Arial"]
+matplotlib.rcParams["figure.figsize"] = (10,8)
 
 str_exp=os.environ['goto_pto_exp']
 os.chdir(str_exp)
 
 str_xheader, list_yheaders, array1d_xdata, array2d_ydata = def_xas_extract( str_datfile='20210924.Pt.110.a20.csv', int_xcolumn=0, list1d_ycolumn=[1] )
-plt.plot( array1d_xdata, array2d_ydata,label='20' )
+plt.plot( array1d_xdata, array2d_ydata,label=r'Exp. 20$\degree$' )
 
 str_xheader, list_yheaders, array1d_xdata, array2d_ydata = def_xas_extract( str_datfile='20210924.Pt.110.a41.csv', int_xcolumn=0, list1d_ycolumn=[1] )
-plt.plot( array1d_xdata, array2d_ydata,label='41' )
+plt.plot( array1d_xdata, array2d_ydata,label=r'Exp. 41$\degree$' )
 
 str_xheader, list_yheaders, array1d_xdata, array2d_ydata = def_xas_extract( str_datfile='xas_exp.xyfit.csv', int_xcolumn=0, list1d_ycolumn=[1] )
 #array2d_ydata.reshape( (len(array1d_xdata)) )
-plt.plot( array1d_xdata, array2d_ydata,label='xyfit' )
+plt.plot( array1d_xdata, array2d_ydata,label=r'Exp. 90$\degree$ fit' )
 
 plt.legend()
 
@@ -26,4 +34,4 @@ plt.xlabel( 'Energy (eV)' )
 plt.ylabel( "Intensity (Arb. Units)" )
 
 plt.show()
-plt.savefig( 'xas_exp.xyfit.pdf' )
+plt.savefig( 'xas_exp.xyfit.pdf',bbox_inches='tight' )
