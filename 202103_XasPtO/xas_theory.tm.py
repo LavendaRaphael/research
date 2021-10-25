9bd72d215c4b4ba24a8e8f632c8043f286445f34
LavendaRaphael <feifei.tian.cn@gmail.com>
2021-10-25 17:12:42 +0800

    vscode

--- 202103_XasPtO/xas_theory.tm.py
+++ 202103_XasPtO/xas_theory.tm.py
@@ -1,9 +1,7 @@
 #!/bin/env python
-import from_xas_modules
+import xas_module
 import os
-import sys
-sys.path.append(r'./')
-from from_dict_structures import dict_structures 
+import local_module
 
 list1d_workdir=[]
 #----------------------------------[Pt.110]
@@ -14,7 +12,11 @@ str_jsonfile='xas.a20_b90.json'
 list2d_angle = []
 list2d_angle.append( [ 90, 45 ] )
 list2d_angle.append( [  0, 90 ] )
+list2d_angle.append( [  0, 90 ] )
+list2d_angle.append( [  0, 90 ] )
 
+
+dict_structures = local_module.def_dict_structures()
 for str_workdir in list1d_workdir:
     str_chdir = dict_structures[ str_workdir ].str_chdir
     str_chdir = str_chdir+'vasp_sch/'
@@ -22,4 +24,8 @@ for str_workdir in list1d_workdir:
     print(os.getcwd())
 
     list2d_atom = dict_structures[ str_workdir ].list2d_atom
-    from_xas_modules.def_xas_atom_abworkflow(  str_jsonfile=str_jsonfile, list2d_angle=list2d_angle, list2d_atom=list2d_atom)
+    xas_module.def_xas_atom_abworkflow(
+        str_jsonfile=str_jsonfile, 
+        list2d_angle=list2d_angle, 
+        list2d_atom=list2d_atom
+        )
