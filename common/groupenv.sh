cfa76ddedd2f69063bec63d157b8335acd158f7c
LavendaRaphael <feifei.tian.cn@gmail.com>
2021-11-30 15:49:10 +0800

    auto

--- common/groupenv.sh
+++ common/groupenv.sh
@@ -14,12 +14,12 @@ echo "#=========================================================================
 #---------------------------------------------[dir]
 export vasp_pot=${homedir}software/potpaw_PBE.54/
 
-export goto_pto_exp=${homedir}group/202103_XasPtO/exp/
-export goto_pto_work_110=${homedir}group/202103_XasPtO/server/Pt.110_O_vac/
-export goto_pto_code=${homedir}codes/group/202103_XasPtO/
-export goto_pto_ms=${homedir}group/202103_XasPtO/structures/Untitled_Files/Documents/
+export goto_pto_exp=${homedir}research/202103_XasPtO/exp/
+export goto_pto_work_110=${homedir}Research/202103_XasPtO/server/Pt.110_O_vac/
+export goto_pto_code=${homedir}codes/research/202103_XasPtO/
+export goto_pto_ms=${homedir}research/202103_XasPtO/structures/Untitled_Files/Documents/
 export goto_pto_comput_110=${homedir}comput/202103_XasPtO/server/Pt.110_O_vac/
-export goto_pto_work_111=${homedir}group/202103_XasPtO/server/Pt.111_O_vac/
+export goto_pto_work_111=${homedir}research/202103_XasPtO/server/Pt.111_O_vac/
 
 #---------------------------------------------[path]
 export PATH=${homedir}software/vtstscripts-967:$PATH
