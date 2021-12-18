42d3771edf99ec1936ac7656e1582a8777e47bbb
LavendaRaphael <feifei.tian.cn@gmail.com>
2021-12-18 20:16:08 +0800

    auto

---
+++ 202103_XasPtO/local_env.sh
@@ -0,0 +1,6 @@
+export goto_pto_exp=${homedir}research/202103_XasPtO/exp/
+export goto_pto_work_110=${homedir}research/202103_XasPtO/server/Pt.110_O_vac/
+export goto_pto_code=${homedir}codes/research/202103_XasPtO/
+export goto_pto_ms=${homedir}research/202103_XasPtO/structures/Untitled_Files/Documents/
+export goto_pto_comput_110=${homedir}comput/202103_XasPtO/server/Pt.110_O_vac/
+export goto_pto_work_111=${homedir}research/202103_XasPtO/server/Pt.111_O_vac/
