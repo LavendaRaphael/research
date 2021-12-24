837241b0e157bc2b9d93e31131f23025382005c1
LavendaRaphael <feifei.tian.cn@gmail.com>
2021-12-24 10:48:17 +0800

    auto

--- common/groupenv.sh
+++ common/groupenv.sh
@@ -7,14 +7,20 @@ fi
 fi
 
 export homedir=`find ~ -maxdepth 3 -name "server.me.sh" -print -quit|xargs dirname`/
-source ${homedir}codes/common/environment.sh
 
 echo "#=========================================================================[groupenv.sh]"
 
 #---------------------------------------------[dir]
 export vasp_pot=${homedir}software/potpaw_PBE.54/
+
 #---------------------------------------------[path]
 export PATH=${homedir}software/vtstscripts-967:$PATH
+
+source $software/bin/kim-api-activate
+
+export PLUMED_KERNEL="${software}/lib/libplumedKernel.so"
+export PLUMED_VIMPATH="${software}/src/plumed2.7/vim"
+export PYTHONPATH="${software}/lib/plumed/python:$PYTHONPATH"
 
 #---------------------------------------------[jobsub]
 if [ "$mycluster" = "qsub" ]; then
