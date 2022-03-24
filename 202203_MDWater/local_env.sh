3f96e1b855123d60cac5236ec1597e6be5e97056
LavendaRaphael <feifei.tian.cn@gmail.com>
2022-03-24 13:51:15 +0800

    auto

--- 202203_MDWater/local_env.sh
+++ 202203_MDWater/local_env.sh
@@ -1,3 +1,3 @@
 export dir_mdwater_code="${homedir}codes/research/202203_MDWater/"
-export dir_mdwater_server="${homedir}research/202203_MDWater/"
+export dir_mdwater_work="${homedir}research/202203_MDWater/"
 export dir_mdwater_storage="${homedir}/storage/202203_MDWater/"
