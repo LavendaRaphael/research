721948b19de3d3809a6b8dfc4497a00e4d5fbc16
LavendaRaphael <feifei.tian.cn@gmail.com>
2022-04-22 21:51:09 +0800

    auto

--- 202203_MDWater/do_lmp2pdb.py
+++ 202203_MDWater/do_lmp2pdb.py
@@ -1,8 +1,9 @@
 import model_devi
 
-array_id = [3800]
+array_id = [10000]
 
 model_devi.def_lmp2pdb(
     array_id,
-    type_map = ["O", "H"]
+    type_map = ["O", "H"],
+    str_save = "10000.pdb"
     )
--- 202203_MDWater/do_model_devi_atom.py
+++ 202203_MDWater/do_model_devi_atom.py
@@ -1,26 +1,27 @@
 
 import dpdata
 import model_devi
+import os
 
-dp_sys = dpdata.System(file_name="traj_bk/3800.lammpstrj", fmt='lammps/dump', type_map=["O","H"])
+dp_sys = dpdata.System(file_name="traj/10000.lammpstrj", fmt='lammps/dump', type_map=["O","H"])
+
+str_save = "devi_f_atom.10000.pdf"
+
+str_dir_iter = "../../"
+#str_dir_iter = "../../../iter.000006/"
+#str_dir_iter = "../../../../dpgen_origin/iter.000005/"
 
 list_model = [
-    "../../00.train/000/frozen_model.pb",
-    "../../00.train/001/frozen_model.pb",
-    "../../00.train/002/frozen_model.pb",
-    "../../00.train/003/frozen_model.pb",
+    os.path.join(str_dir_iter, "00.train/000/frozen_model.pb"),
+    os.path.join(str_dir_iter, "00.train/001/frozen_model.pb"),
+    os.path.join(str_dir_iter, "00.train/002/frozen_model.pb"),
+    os.path.join(str_dir_iter, "00.train/003/frozen_model.pb"),
     ]
-'''
-list_model = [
-    "../../../../dpgen_origin/iter.000006/00.train/000/frozen_model.pb",
-    "../../../../dpgen_origin/iter.000006/00.train/001/frozen_model.pb",
-    "../../../../dpgen_origin/iter.000006/00.train/002/frozen_model.pb",
-    "../../../../dpgen_origin/iter.000006/00.train/003/frozen_model.pb",
-    ]
-'''
+
 model_devi.def_model_devi_atom(
     list_model = list_model,
     dp_sys = dp_sys,
-    float_lowthred = 0.25,
-    str_save = "devi_f_atom.3800.pdf"
+    float_lowthred = 0.15,
+    str_save = str_save,
+    #float_ylim = 2.6,
     )
