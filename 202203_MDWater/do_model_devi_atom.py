3e0e3ca31c0bcd63959564d6cce4ed869333fd44
LavendaRaphael <feifei.tian.cn@gmail.com>
2022-04-23 22:30:59 +0800

    auto

--- 202203_MDWater/do_model_devi_atom.py
+++ 202203_MDWater/do_model_devi_atom.py
@@ -3,12 +3,13 @@ import dpdata
 import model_devi
 import os
 
-dp_sys = dpdata.System(file_name="traj/10000.lammpstrj", fmt='lammps/dump', type_map=["O","H"])
+dp_sys = dpdata.System(file_name="traj.bk/10000.lammpstrj", fmt='lammps/dump', type_map=["O","H"])
 
-str_save = "devi_f_atom.10000.pdf"
+str_save = "devi_f_atom.iter6.bk.10000.pdf"
 
-str_dir_iter = "../../"
+#str_dir_iter = "../../"
 #str_dir_iter = "../../../iter.000006/"
+str_dir_iter = "../../../iter.000006.bk/"
 #str_dir_iter = "../../../../dpgen_origin/iter.000005/"
 
 list_model = [
