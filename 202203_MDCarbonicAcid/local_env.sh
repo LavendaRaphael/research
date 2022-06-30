80025c9d4c2b8c9d116f8276722ac15ab3988c0a
LavendaRaphael <feifei.tian.cn@gmail.com>
2022-06-30 14:13:22 +0800

    auto

--- 202203_MDCarbonicAcid/local_env.sh
+++ 202203_MDCarbonicAcid/local_env.sh
@@ -1,4 +1,5 @@
 export dir_mdca_code="${homedir}codes/research/202203_MDCarbonicAcid/"
 export dir_mdca_work="${homedir}research/202203_MDCarbonicAcid/"
 export dir_mdca_shtu="${homedir}research_shtu/202203_MDCarbonicAcid/"
+export dir_mdca_d="${homedir}research_d/202203_MDCarbonicAcid/"
 export dir_mdca_ms="${homedir}research/202203_MDCarbonicAcid/structure/H2CO3_Files/Documents/"
