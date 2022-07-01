abfd179a019a9b04d2cf9a99ffc4de9d175c5972
LavendaRaphael <feifei.tian.cn@gmail.com>
2022-07-01 19:51:01 +0800

    auto

--- common/cuda_auto.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-import os
-
-subprocess_qstat = subprocess.run( args=['qstat -f ${PBS_JOBID}|grep exec_gpus'], shell=True ,stdout=subprocess.PIPE, encoding="utf-8")
-list_gpu = subprocess_qstat.stdout.split(' ')[-1][:-1].split('+')
-print( list_gpu[ int(os.environ['CUDA_VISIBLE_DEVICES']) ].split('/')[-1] )
--- common/cuda_auto.sh
+++ common/cuda_auto.sh
@@ -1,6 +1,15 @@
-echo "export: `export|grep CUDA_VISIBLE_DEVICES`"
+qstat -f ${PBS_JOBID}|grep exec_gpus
 
-exec_gpus=`qstat -f ${PBS_JOBID}|grep exec_gpus`
-export CUDA_VISIBLE_DEVICES=${exec_gpus##*/}
+#export CUDA_VISIBLE_DEVICES=`python $homedir/codes/research/common/cuda_auto.py`
+export CUDA_VISIBLE_DEVICES=`python << EOF
+import subprocess
+import os
 
-echo "CUDA_VISIBLE_DEVICES set to: ${CUDA_VISIBLE_DEVICES}"
+subprocess_qstat = subprocess.run( args=["qstat -f ${PBS_JOBID}|grep exec_gpus"], shell=True ,stdout=subprocess.PIPE, encoding="utf-8")
+list_gpu = subprocess_qstat.stdout.split(" ")[-1][:-1].split("+")
+print( list_gpu[ int(os.environ["CUDA_VISIBLE_DEVICES"]) ].split("/")[-1] )
+EOF
+`
+
+echo CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES 
+
